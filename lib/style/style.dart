6d57338b2614ce187f3d2639867a4f3f226ed9c0
rafareloM <rafarelomorais@gmail.com>
2022-10-25 22:08:48 -0300

    Revert "refatoração de acordo com o linter"
    This reverts commit a2dc22c3f2a9ecf2b31bfb26fda4fc827a59d966.

--- lib/style/style.dart
+++ lib/style/style.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppStyle {
-  static Color backgroundColor = const Color(0xffd6d4d0);
-  static Color homepageColor = const Color(0xffefede9);
-  static Color button1Color = const Color(0xff46f8a3);
-  static Color backgroundBuyColor = const Color(0xff100607);
-  static Color icon1Color = const Color(0xff258afe);
-  static Color icon2Color = const Color(0xff3dffdc);
-  static Color icon3Color = const Color(0xff47f7a3);
-  static Color cardBackgroundColor = const Color(0xffffffff);
-  static Color icon4Color = const Color(0xff100705);
+  static Color backgroundColor = Color(0xffd6d4d0);
+  static Color homepageColor = Color(0xffefede9);
+  static Color button1Color = Color(0xff46f8a3);
+  static Color backgroundBuyColor = Color(0xff100607);
+  static Color icon1Color = Color(0xff258afe);
+  static Color icon2Color = Color(0xff3dffdc);
+  static Color icon3Color = Color(0xff47f7a3);
+  static Color cardBackgroundColor = Color(0xffffffff);
+  static Color icon4Color = Color(0xff100705);
 }
