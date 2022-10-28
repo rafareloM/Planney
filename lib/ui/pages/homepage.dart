60b3eafc8ea81b793da72b021eeae8bc8e884969
rafareloM <rafarelomorais@gmail.com>
2022-10-28 03:40:59 -0300

    implementa o widget HomePage

--- lib/ui/pages/homepage.dart
+++ lib/ui/pages/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:planney/style/style.dart';
 import 'package:planney/ui/components/avatar.dart';
+import 'package:planney/ui/components/bottomNavigationBar.dart';
 import 'package:planney/ui/components/info_card.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,19 +10,61 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppStyle.backgroundColor,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        backgroundColor: AppStyle.button1Color,
+        label: Text(
+          'Nova Transição',
+          style: TextStyle(
+            color: AppStyle.backgroundBuyColor,
+          ),
+        ),
+      ),
+      backgroundColor: AppStyle.homepageColor,
+      appBar: AppBar(
+          title: Text(
+            'Planney',
+            style: TextStyle(
+              color: AppStyle.button1Color,
+              fontSize: 24,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: AppStyle.backgroundBuyColor,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.menu,
+              color: AppStyle.button1Color,
+            ),
+          )),
+      bottomNavigationBar: const HomeBottomNavigationBar(),
       body: Column(
         children: [
-          Avatar(
-            userName: 'Marcus',
-            userBalance: 2365.96,
-          ),
-          SingleChildScrollView(
-            child: InfoCard(
-                cardTitle: 'Fluxo de Gastos',
-                textButtonCTA: 'Exibir Detalhes',
-                imagePath:
-                    'lib/style/assets/img/Colorful Flower Fashion Sale Presentation (3).png'),
+          Expanded(
+            child: ListView(
+              children: const [
+                Avatar(
+                  userName: 'Marcus',
+                  userBalance: 2365.96,
+                ),
+                InfoCard(
+                    cardTitle: 'Fluxo do Saldo',
+                    textButtonCTA: 'Mais Detalhes',
+                    imagePath:
+                        'lib/style/assets/img/Colorful Flower Fashion Sale Presentation (3).png'),
+                InfoCard(
+                    cardTitle: 'Despesas Semanais',
+                    textButtonCTA: 'Exibir Transações',
+                    imagePath:
+                        'lib/style/assets/img/Green and Orange Budget Pie Chart Graph (1).png'),
+                InfoCard(
+                    cardTitle: 'Despesas Pendentes',
+                    textButtonCTA: 'Mostrar Despesas',
+                    imagePath:
+                        'lib/style/assets/img/Green Minimalist 4 Points Donut Chart Graph (2).png'),
+              ],
+            ),
           )
         ],
       ),
