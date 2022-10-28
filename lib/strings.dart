5aa81535e06488500ccac74038475fb75804bbb5
Rafael Morais <86268949+rafareloM@users.noreply.github.com>
2022-10-28 06:41:44 -0300

    feature/welcomePage (#28)
    * implementa welcomePage e arquivo Strings
    
    * correcoes menores

---
+++ lib/strings.dart
@@ -0,0 +1,5 @@
+abstract class Strings {
+  static const String LOGIN_TITLE = 'Gerenciamento\nSimplificado';
+  static const String LOGIN_DESCRIPTION =
+      'Gerencie de forma simples os\nseus gastos e investimentos.';
+}
