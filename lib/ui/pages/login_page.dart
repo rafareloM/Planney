1884b471eb9f3af9d66016e99c9001c376b22b44
Rafael Morais <86268949+rafareloM@users.noreply.github.com>
2022-12-01 21:04:32 -0300

    Update login_page.dart

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,147 +1,97 @@
 import 'package:flutter/material.dart';
-import 'package:planney/extensions/extensions_string.dart';
-import 'package:planney/model/user_credential.dart';
 import 'package:planney/strings.dart';
 import 'package:planney/style/style.dart';
-import 'package:planney/ui/components/planney_logo.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
-
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<WelcomePage> createState() => WelcomePageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  bool _canShowPassword = false;
-  String _email = '';
-  String _password = '';
-
-  UserCredential? _userCredential;
-
-  final _formKey = GlobalKey<FormState>();
+class WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
+    double deviceWidht = MediaQuery.of(context).size.width;
     double deviceHeight = MediaQuery.of(context).size.height;
-    double deviceWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      backgroundColor: AppStyle.backgroundBuyColor,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: deviceHeight * 0.15,
-                ),
-                const PlanneyLogo(),
-                SizedBox(height: deviceHeight * 0.1),
-                SizedBox(
-                  width: deviceWidth * 0.9,
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: AppStyle.button1Color,
-                            width: 1,
-                            style: BorderStyle.solid),
-                        borderRadius: const BorderRadius.vertical(
-                          top: Radius.circular(12.5),
-                        ),
-                      ),
-                      labelText: Strings.LOGIN_FORM_EMAIL_LABEL,
-                      labelStyle: TextStyle(
-                          color: AppStyle.backgroundBuyColor, fontSize: 18),
-                      errorStyle: TextStyle(
-                          color: AppStyle.cardBackgroundColor, fontSize: 14),
-                      fillColor: AppStyle.cardBackgroundColor,
-                      filled: true,
-                    ),
-                    validator: ((value) {
-                      if (!value!.isValidEmail) {
-                        return Strings.LOGIN_FORM_EMAIL_ERROR;
-                      }
-                      return null;
-                    }),
-                    onSaved: (newValue) => _email = newValue!,
-                  ),
-                ),
-                SizedBox(height: deviceHeight * 0.04),
-                SizedBox(
-                  width: deviceWidth * 0.9,
-                  child: TextFormField(
-                    obscureText: !_canShowPassword,
-                    decoration: InputDecoration(
-                      border: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: AppStyle.button1Color,
-                            width: 1,
-                            style: BorderStyle.solid),
-                        borderRadius: const BorderRadius.vertical(
-                          bottom: Radius.circular(12.5),
-                        ),
-                      ),
-                      labelText: Strings.LOGIN_FORM_PASSWORD_LABEL,
-                      labelStyle: TextStyle(color: AppStyle.backgroundBuyColor),
-                      helperText: Strings.LOGIN_FORM_PASSWORD_HELPER,
-                      helperStyle: TextStyle(
-                          color: AppStyle.cardBackgroundColor, fontSize: 14),
-                      fillColor: AppStyle.cardBackgroundColor,
-                      errorStyle: TextStyle(
-                          color: AppStyle.cardBackgroundColor, fontSize: 14),
-                      filled: true,
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(
-                            () {
-                              _canShowPassword = !_canShowPassword;
-                            },
-                          );
-                        },
-                        icon: Icon(_canShowPassword
-                            ? Icons.visibility
-                            : Icons.visibility_off),
-                      ),
-                    ),
-                    validator: ((value) {
-                      if (!value!.isValidPassword) {
-                        return Strings.LOGIN_FORM_PASSWORD_ERROR;
-                      }
-                      return null;
-                    }),
-                    onSaved: (newValue) => _password = newValue!,
-                  ),
-                ),
-                SizedBox(height: deviceHeight * 0.08),
-                SizedBox(
-                  width: deviceWidth * 0.9,
-                  height: 48,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        Navigator.popAndPushNamed(
-                          context,
-                          '/',
-                          arguments: _userCredential,
-                        );
-                      }
-                    },
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(AppStyle.button1Color),
-                        foregroundColor: MaterialStateProperty.all(
-                            AppStyle.backgroundBuyColor)),
-                    child: const Text(
-                      Strings.LOGIN_BUTTON_ENTER,
-                      style: TextStyle(fontSize: 18),
+    return Material(
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24,
+            vertical: 24,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Planney',
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
+              ),
+              SizedBox(
+                height: deviceHeight * 0.1,
+              ),
+              Stack(
+                children: [
+                  Container(
+                    height: 56,
+                    width: 56,
+                    decoration: BoxDecoration(
+                      color: AppStyle.fullWhite,
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(100)),
                     ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 42),
+                    child: Container(
+                      height: 56,
+                      width: 56,
+                      decoration: BoxDecoration(
+                        color: AppStyle.primaryColor,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(100)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: deviceHeight * 0.025,
+              ),
+              const Text(
+                Strings.WELCOME_TITLE,
+                style: TextStyle(fontSize: 42, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: deviceHeight * 0.0125,
+              ),
+              const Text(
+                Strings.WELCOME_DESCRIPTION,
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.35,
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.popAndPushNamed(context, '/loginPage');
+                  },
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      const StadiumBorder(),
+                    ),
+                    fixedSize: MaterialStateProperty.all(
+                      Size(deviceWidht * 0.35, deviceHeight * 0.06),
+                    ),
+                  ),
+                  child: const Text(
+                    'SIGN UP',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
