8346575bb81e40c4cacb7089f4893af1108287ef
marcuscabrall <marcus_vinicius.c@hotmail.com>
2023-01-08 15:27:21 -0300

    Calendar: date format and language update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
 import 'package:planney/ui/controller/home.controller.dart';
@@ -19,15 +20,19 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-  
     final controller = GetIt.instance.get<HomePageController>();
 
     return Observer(builder: (context) {
       return MaterialApp(
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate
+        ],
+        supportedLocales: const [Locale('pt', 'BR')],
         debugShowCheckedModeBanner: false,
         title: 'Planney',
         theme: controller.selectedAppTheme,
-        initialRoute: '/welcomePage',
+        initialRoute: '/',
         routes: {
           '/': ((context) => const HomePage()),
           '/welcomePage': ((context) => const WelcomePage()),
