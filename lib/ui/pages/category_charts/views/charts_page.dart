bc43975467e5ac314a55e81eb17c2e7aa4dc90e0
Rafael Morais <86268949+rafareloM@users.noreply.github.com>
2023-02-02 20:26:36 -0300

    implements charts_page

--- lib/ui/pages/category_charts/views/charts_page.dart
+++ lib/ui/pages/category_charts/views/charts_page.dart
@@ -1,52 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:planney/mocks/transaction_mock.model.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:get_it/get_it.dart';
 import 'package:planney/model/transaction.model.dart';
 import 'package:planney/navigator_key.dart';
+import 'package:planney/style/style.dart';
 import 'package:planney/ui/components/charts/bar_chart.dart';
 import 'package:planney/ui/components/charts/pie_chart_category.dart';
 import 'package:planney/ui/components/home/info_card.dart';
+import 'package:planney/ui/controller/home.controller.dart';
 import 'package:planney/ui/pages/category_charts/views/category_details_page.dart';
 
 class CategoryChartsPage extends StatelessWidget {
-  final List<Transaction> transactions;
   final String categoryName;
+  final List<Transaction> list;
   const CategoryChartsPage({
     super.key,
-    required this.transactions,
     required this.categoryName,
+    required this.list,
   });
 
   @override
   Widget build(BuildContext context) {
+    final HomePageController controller =
+        GetIt.instance.get<HomePageController>();
+
     ColorScheme colorScheme = Theme.of(context).colorScheme;
     Color textColor = colorScheme.brightness == Brightness.dark
         ? colorScheme.onSurface
         : colorScheme.primary;
 
-    TransactionType? type = transactions.isEmpty
+    TransactionType? type = controller.finalListExpence.isEmpty
         ? TransactionType.expence
-        : transactions.first.type;
+        : controller.finalListExpence.first.type;
 
-    Widget pieChart = PieChartCategory(
+    Widget pieChart = Observer(builder: (context) {
+      return PieChartCategory(
         titleColor: textColor,
-        transactionsList: TransactionMock.transactionsMock
-        //TODO: Implement reactive transactions.
+        transactionsList: controller.filteredList
+            .takeWhile((value) => value.date
+                .isAfter(DateTime.now().subtract(const Duration(days: 8))))
+            .toList(),
+      );
+    });
 
-        // transactions
-        //     .takeWhile((value) => value.date.isAfter(
-        //         DateTime.now().subtract(const Duration(days: 8))))
-        //     .toList(),
-        );
-
-    Widget barChart = BarChartSample3(
-      textColor: textColor,
-      //TODO: Implement reactive transactions.
-
-      // transactions
-      //     .takeWhile((value) => value.date.isAfter(
-      //         DateTime.now().subtract(const Duration(days: 8))))
-      //     .toList(),
-    );
+    Widget barChart = Observer(builder: (context) {
+      return BarChartSample3(
+        textColor: textColor,
+        transactions: list
+            .takeWhile((value) => value.date
+                .isAfter(DateTime.now().subtract(const Duration(days: 8))))
+            .toList(),
+      );
+    });
 
     String pieChartTitle =
         type == TransactionType.expence ? 'Despesa Semanal' : 'Receita Semanal';
@@ -89,22 +94,60 @@ class CategoryChartsPage extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(bottom: 16),
-              child: InfoCard(
-                  height: deviceHeight * 0.48,
-                  textButtonCTA: 'Mais detalhes',
-                  content: pieChart,
-                  onPressed: () {
-                    Navigator.push(
-                      navigatorKey.currentContext!,
-                      MaterialPageRoute(
-                        builder: (context) => CategoryDetailsPage(
-                          chart: pieChart,
-                          title: pieChartTitle,
+              child: list.isEmpty
+                  ? InfoCard(
+                      height: deviceHeight * 0.48,
+                      textButtonCTA: 'Mais detalhes',
+                      content: Center(
+                        child: Stack(
+                          children: [
+                            Container(
+                              height: deviceHeight * 0.3,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: AppStyle.initialchartcolor,
+                              ),
+                              child: const Padding(
+                                padding: EdgeInsets.all(36),
+                                child: Card(
+                                  margin: EdgeInsets.zero,
+                                  shape: CircleBorder(),
+                                  child: Center(
+                                    child: Text(
+                                      'A categoria\nestá vazia!',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    );
-                  },
-                  buttonColor: textColor),
+                      onPressed: null,
+                      buttonColor: textColor)
+                  : InfoCard(
+                      height: deviceHeight * 0.48,
+                      textButtonCTA: 'Mais detalhes',
+                      content: pieChart,
+                      onPressed: () {
+                        controller.getTransactionsByCategory(categoryName);
+                        Navigator.push(
+                          navigatorKey.currentContext!,
+                          MaterialPageRoute(
+                            builder: (context) => CategoryDetailsPage(
+                              transactionList: controller.filteredList,
+                              chart: pieChart,
+                              title: pieChartTitle,
+                            ),
+                          ),
+                        );
+                      },
+                      buttonColor: textColor),
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(16, 0, 0, 16),
@@ -119,22 +162,59 @@ class CategoryChartsPage extends StatelessWidget {
                 ),
               ),
             ),
-            InfoCard(
-                height: deviceHeight * 0.48,
-                textButtonCTA: 'Mais detalhes',
-                content: barChart,
-                onPressed: () {
-                  Navigator.push(
-                    navigatorKey.currentContext!,
-                    MaterialPageRoute(
-                      builder: (context) => CategoryDetailsPage(
-                        chart: barChart,
-                        title: barChartTitle,
+            list.isEmpty
+                ? InfoCard(
+                    height: deviceHeight * 0.48,
+                    textButtonCTA: 'Mais detalhes',
+                    content: Center(
+                      child: Stack(
+                        children: [
+                          Container(
+                            height: deviceHeight * 0.3,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: AppStyle.initialchartcolor,
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.all(36),
+                              child: Card(
+                                margin: EdgeInsets.zero,
+                                shape: CircleBorder(),
+                                child: Center(
+                                  child: Text(
+                                    'A categoria\nestá vazia!',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  );
-                },
-                buttonColor: textColor),
+                    onPressed: null,
+                    buttonColor: textColor)
+                : InfoCard(
+                    height: deviceHeight * 0.48,
+                    textButtonCTA: 'Mais detalhes',
+                    content: barChart,
+                    onPressed: () {
+                      Navigator.push(
+                        navigatorKey.currentContext!,
+                        MaterialPageRoute(
+                          builder: (context) => CategoryDetailsPage(
+                            chart: barChart,
+                            title: barChartTitle,
+                            transactionList: list,
+                          ),
+                        ),
+                      );
+                    },
+                    buttonColor: textColor),
             SizedBox(
               height: deviceHeight * 0.025,
             )
